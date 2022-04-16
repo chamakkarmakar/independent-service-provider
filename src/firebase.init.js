84a89dab9d018ab28480b3ff7739d9dd05b8171f
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-04-17 00:24:38 +0600

    firebase & react-router installed

---
+++ src/firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDU1Xp4RexbcwMQnR65YpbF-M-B8Mq5oXw",
+    authDomain: "independent-service-provider-1.firebaseapp.com",
+    projectId: "independent-service-provider-1",
+    storageBucket: "independent-service-provider-1.appspot.com",
+    messagingSenderId: "204358866301",
+    appId: "1:204358866301:web:8766b9a1dcd468ff3e6814"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
