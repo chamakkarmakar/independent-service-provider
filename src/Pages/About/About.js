94b0eb83c62437597f8792c28f3386c8d7187875
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-04-18 21:14:42 +0600

    created About Me

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import mypic from '../../img/mypic.jpg';
 import './About.css';
 
 const About = () => {
     return (
         <div>
-            <h5>About Me</h5>
+            <h1>About Me</h1>
+            <div className="about-me">
+                <div className='details about-half'>
+                    <h3>Chamak Karmakar</h3>
+                    <p>I'm looking for an opportunity to grow
+                        myself and develop my skill as well. As a
+                        computer science graduate, I am a tech
+                        enthusiast and have good command over
+                        programming language. My passion for
+                        working as a full stack developer has driven me
+                        to acquire new knowledge about web
+                        development and it methodologies.</p>
+                </div>
+                <div className='picture about-half'>
+                    <img src={mypic} alt="mypic" height={300} width={300} />
+                </div>
+            </div>
         </div>
     );
 };
